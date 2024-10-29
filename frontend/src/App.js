fecd05253d760885933d4f9978bb684339b7db83
gabriel talih <gtalih@yahoo.com>
2024-10-28 22:26:46 -0400

    Created frontend webapp

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>We did it gang</p>
+        <p>Let's get it</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
