d45ff2f1306c6e709eb9c08d8671bda1a2759cfd
gabriel talih <gtalih@yahoo.com>
2024-11-14 23:31:56 -0500

    Adding Spring Boot to my life

---
+++ backend/src/main/java/com/example/demo/BackendForGroupCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendForGroupCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendForGroupCodeApplication.class, args);
+	}
+
+}
