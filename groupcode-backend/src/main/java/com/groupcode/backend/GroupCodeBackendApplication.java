f587ad058e6d1631aede8761c20a51b481f28062
gabriel talih <gtalih@yahoo.com>
2024-11-27 15:03:17 -0500

    added spring boot backend

---
+++ groupcode-backend/src/main/java/com/groupcode/backend/GroupCodeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.groupcode.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupCodeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupCodeBackendApplication.class, args);
+	}
+
+}
---
+++ groupcode-backend/src/test/java/com/groupcode/backend/GroupCodeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupcode.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupCodeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
